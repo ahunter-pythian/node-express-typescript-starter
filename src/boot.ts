557778a172f56fbe31a836d244d8bf90e6fe46b9
Anthony Hunter <anthony@redbat.ca>
2019-01-09 20:43:18 -0500

    Added application boot

---
+++ src/boot.ts
@@ -0,0 +1,14 @@
+/*************************************************************************************************************************
+ * Copyright (c) 2019, Anthony Hunter.
+ * This unpublished material is proprietary to Anthony Hunter.
+ * All rights reserved. The methods and techniques described herein are considered trade secrets and/or confidential.
+ * Reproduction or distribution, in whole or in part, is forbidden except by express written permission of Anthony
+ * Hunter.
+ *************************************************************************************************************************/
+import {Application} from "./Application";
+
+const bootstrap = () => {
+    Application.start();
+};
+
+bootstrap();
